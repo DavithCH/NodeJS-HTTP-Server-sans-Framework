22a22052573168e258f81a27e345780a776fb005
Davith Chhung <chhungdavith@gmail.com>
2022-05-02 15:37:26 +0200

    Fini d'implémenter la route pour tous les chemins qui ne sont pas bon en renvoyant le STATUS CODE 404 et modifier modifier la route précédente en mettent le STATUS CODE 401

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = http.createServer((req, res) => {
   try {
     if (req.url === "/") {
       if (req.method !== "GET") {
-        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.writeHead(401, { "Content-Type": "text/plain" });
         res.write("<h1>404 Méthode non autorisée</h1>");
         res.end();
       } else {
@@ -11,6 +11,10 @@ const server = http.createServer((req, res) => {
         res.write("<h1>HELLO WORLD DAVITH CHHUNG</h1>");
         res.end();
       }
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>404 Page introuvable</h1>");
+      res.end();
     }
   } catch (err) {
     console.log(err);
