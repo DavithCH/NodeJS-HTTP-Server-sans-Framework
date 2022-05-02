9e89f5bd055126296c168879e1c980cd71e15251
Davith Chhung <chhungdavith@gmail.com>
2022-05-02 15:24:53 +0200

    Fini d'implémenter le STATUS CODE 401 unautorized pour tous les requêtes qui ne sont pas GET à /

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   try {
     if (req.url === "/") {
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write("<h1>HELLO WORLD DAVITH CHHUNG</h1>");
-      res.end();
+      if (req.method !== "GET") {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.write("<h1>404 Méthode non autorisée</h1>");
+        res.end();
+      } else {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write("<h1>HELLO WORLD DAVITH CHHUNG</h1>");
+        res.end();
+      }
     }
   } catch (err) {
     console.log(err);
